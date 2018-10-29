b854cdfb209c82e935f16b1e643e2ac263e33fa9
ivan4000 <sxln1@mail.ru>
2018-10-29 13:49:06 +0300

    JL37ifElse final final  commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
 public class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 6;
+        int c = 7;
+        int d = 9;
+        boolean bl = a == b;
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a == b");
+        }
+
+        if (a == b) {
+            if (c > d) {
+                System.out.println("c>d");
+            } else {
+                System.out.println("c<d");
+            }
+        }
+    }
 }
